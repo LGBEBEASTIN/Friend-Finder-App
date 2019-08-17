70e14e39a94d56b27a4919592be465aa8079096e
Lawrence Gober <goberlawrence@icloud.com>
2019-08-17 14:48:52 -0400

    Object of arrays set up and home html set up

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+let friends = [
+    {
+        name: "Monkey D. Luffy",
+        photo: "https://onepiece.fandom.com/wiki/Monkey_D._Luffy",
+        scores: [5,4,2,1,5,1,1,5,5,5]
+    },
+    {
+        name: "Roronoa Zoro",
+        photo: "https://onepiece.fandom.com/wiki/Roronoa_Zoro",
+        scores: [3,1,5,1,3,5,1,5,5,4]
+    },
+    {
+        name: "Vinsmoke Sanji",
+        photo: "https://onepiece.fandom.com/wiki/Sanji",
+        scores: [3,1,1,5,3,2,5,5,5,3]
+    }
+]
