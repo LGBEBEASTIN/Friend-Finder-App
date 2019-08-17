d15abdde0053cb6653caa5f3f705a5cd26ce9bec
Lawrence Gober <goberlawrence@icloud.com>
2019-08-17 13:40:03 -0400

    Server Set Up

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 let express = require("express");
+
 let app = express();
+
+let PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App Listening On PORT: " + PORT);
+  });
