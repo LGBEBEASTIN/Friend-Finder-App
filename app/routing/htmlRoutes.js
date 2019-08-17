e2240907cb238fe83131d669b5873fdc60aa79b1
Lawrence Gober <goberlawrence@icloud.com>
2019-08-17 15:26:57 -0400

    html routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
       res.sendFile(path.join(__dirname, "./public/survey.html"));
     });
   
-    app.get("/home", function(req, res) {
+    app.use(function(req, res) {
       res.sendFile(path.join(__dirname, "./public/home.html"));
     });
   
