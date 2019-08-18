5139aee4b61299d77cae69fbcbd7039695cb6375
Lawrence Gober <goberlawrence@icloud.com>
2019-08-18 18:02:12 -0400

    Changed friend values and make images fit inside of modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,12 +2,12 @@ let friends = [
     {
         name: "Monkey D. Luffy",
         photo: "https://vignette.wikia.nocookie.net/onepiece/images/6/6d/Monkey_D._Luffy_Anime_Post_Timeskip_Infobox.png/revision/latest?cb=20190124235416",
-        scores: [5,4,2,1,5,1,1,5,5,5]
+        scores: [5,5,1,1,5,1,1,5,5,5]
     },
     {
         name: "Roronoa Zoro",
         photo: "https://vignette.wikia.nocookie.net/onepiece/images/5/52/Roronoa_Zoro_Anime_Post_Timeskip_Infobox.png/revision/latest?cb=20141008195159",
-        scores: [3,1,5,1,3,5,1,5,5,4]
+        scores: [4,1,5,1,3,5,1,5,5,5]
     },
     {
         name: "Vinsmoke Sanji",
