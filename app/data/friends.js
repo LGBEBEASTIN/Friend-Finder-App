f7af0f0f9f5a2c0030bc0852fbf4fc25f251bb47
Lawrence Gober <goberlawrence@icloud.com>
2019-08-18 17:40:33 -0400

    Trying to make images scale down using css

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,17 @@
 let friends = [
     {
         name: "Monkey D. Luffy",
-        photo: "https://onepiece.fandom.com/wiki/Monkey_D._Luffy?file=Monkey_D._Luffy_Anime_Post_Timeskip_Infobox.png",
+        photo: "https://vignette.wikia.nocookie.net/onepiece/images/6/6d/Monkey_D._Luffy_Anime_Post_Timeskip_Infobox.png/revision/latest?cb=20190124235416",
         scores: [5,4,2,1,5,1,1,5,5,5]
     },
     {
         name: "Roronoa Zoro",
-        photo: "https://onepiece.fandom.com/wiki/Roronoa_Zoro",
+        photo: "https://vignette.wikia.nocookie.net/onepiece/images/5/52/Roronoa_Zoro_Anime_Post_Timeskip_Infobox.png/revision/latest?cb=20141008195159",
         scores: [3,1,5,1,3,5,1,5,5,4]
     },
     {
         name: "Vinsmoke Sanji",
-        photo: "https://onepiece.fandom.com/wiki/Sanji",
+        photo: "https://vignette.wikia.nocookie.net/onepiece/images/b/b6/Sanji_Anime_Post_Timeskip_Infobox.png/revision/latest?cb=20170625125657",
         scores: [3,1,1,5,3,2,5,5,5,3]
     }
 ]
